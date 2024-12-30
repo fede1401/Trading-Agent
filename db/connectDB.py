7de6355576e9f47764903f3fd0a69bb31e3a482c
Federico Ferdinandi <ferdinandi.1958589@studenti.uniroma1.it>
2024-12-31 00:19:48 +0100

    Revert "Test for commit"
    This reverts commit fc17f0a2c0a26e6e24ea2352dbedd9b748f80ccc.

--- db/connectDB.py
+++ db/connectDB.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-import psycopg2  # https://www.youtube.com/watch?v=miEFm1CyjfM
+import psycopg2 # https://www.youtube.com/watch?v=miEFm1CyjfM
 
 
 # Funzione per connettersi al database PostgreSQL 'nasdaq'.
@@ -7,13 +7,13 @@ def connect_nasdaq():
     try:
         # Configura i parametri di connessione per PostgreSQL
         conn = psycopg2.connect(
-            dbname="nasdaq3",  # Nome del database
-            user="federico",  # Nome utente
-            password="fede",  # Password utente
-            host="localhost",  # Indirizzo IP o nome host del server PostgreSQL
-            port="5433"  # Porta di default per PostgreSQL
+             dbname="nasdaq3",     # Nome del database
+            user="federico",     # Nome utente
+            password="fede",    # Password utente
+            host="localhost",    # Indirizzo IP o nome host del server PostgreSQL
+            port="5433"          # Porta di default per PostgreSQL
         )
-
+        
         # Crea un oggetto cursore per eseguire le query
         cur = conn.cursor()
 
@@ -25,32 +25,33 @@ def connect_nasdaq():
     return cur, conn
 
 
+
 # psql -U federico14 -h localhost -d nasdaq
 
-# Funzione di esempio per connettersi al database 'nasdaq' ed eseguire una query di esempio.
-def connectDB_example():
+# Funzione di esempio per connettersi al database 'nasdaq' e eseguire una query di esempio.
+def connectDB_example ():
     try:
         # Configura i parametri di connessione
         conn = psycopg2.connect(
-            dbname="nasdaq",  # Nome del database
-            user="federico",  # Nome utente
-            password="47002",  # Password utente
-            host="localhost",  # Indirizzo IP o nome host del server PostgreSQL
-            port="5432"  # Porta di default per PostgreSQL
+            dbname="nasdaq",     # Nome del database
+            user="federico",     # Nome utente
+            password="47002",    # Password utente
+            host="localhost",    # Indirizzo IP o nome host del server PostgreSQL
+            port="5432"          # Porta di default per PostgreSQL
         )
-
+        
         # Crea un oggetto cursore per eseguire le query
         cur = conn.cursor()
-
+        
         # Esegui una query di esempio per ottenere la versione del database PostgreSQL
         cur.execute("SELECT version();")
-
+        
         # Recupera il primo record restituito dalla query
         record = cur.fetchone()
 
         # Stampa la versione del database PostgreSQL a cui si è connessi
         print("\nConnesso a - \n", record)
-
+        
         # Chiudi il cursore e la connessione
         cur.close()
         conn.close()
@@ -63,6 +64,7 @@ def connectDB_example():
     return cur, conn
 
 
+
 if __name__ == "__main__":
-    # connectDB_example()
+    #connectDB_example()
     connect_nasdaq()
